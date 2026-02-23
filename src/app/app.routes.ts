839e0726973cdf1d9802371f7ec33300ad219344
Georgi Anastasov <ggeorgianastasov@gmail.com>
2026-02-23 21:35:26 +0200

    Implement initial routing and layout structure for the app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home';
+import { StatsComponent } from './stats/stats';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'stats', component: StatsComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' },
+];
