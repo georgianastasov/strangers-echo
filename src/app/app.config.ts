ac566bd4503c6122a745b38946e79d20f319f801
Georgi Anastasov <ggeorgianastasov@gmail.com>
2026-02-23 21:36:12 +0200

    Integrate Firebase and Firestore into the app

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,23 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBf8PhQsBCGK3RdxgmV0k9ezqqsLTEVLxU",
+  authDomain: "strangers-echo.firebaseapp.com",
+  projectId: "strangers-echo",
+  storageBucket: "strangers-echo.firebasestorage.app",
+  messagingSenderId: "44352452787",
+  appId: "1:44352452787:web:9a99d3d61c7859ac7b98c6"
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore())
   ]
 };
